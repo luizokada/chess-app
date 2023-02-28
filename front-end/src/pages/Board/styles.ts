8ed9832c83560a55050109a4647ce6250e22b7b4
luizokada <luizfernandookada@gmail.com>
2023-02-28 14:57:31 -0300

    fix: style Board

--- front-end/src/pages/Board/styles.ts
+++ front-end/src/pages/Board/styles.ts
@@ -1,22 +1,32 @@
 import styled from 'styled-components';
 
+interface SquareProps {
+    image: string;
+}
 export const Container = styled.div`
     width: 100%;
     height: 100%;
-    display: flex;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
     margin: auto;
 `;
 
-export const BlackSquare = styled.div`
+export const BlackSquare = styled.div<SquareProps>`
     width: 100px;
     height: 100px;
     background-color: green;
+    background-image: url(${(props) => props.image});
     border: 0.5px black solid;
+    background-size: 100%;
+    cursor: ${(props) => (props.image !== '' ? 'pointer' : '')};
 `;
 
-export const WhiteSquare = styled.div`
+export const WhiteSquare = styled.div<SquareProps>`
     width: 100px;
     height: 100px;
+    background-size: 100%;
     background-color: white;
+    background-image: url(${(props) => props.image});
     border: 0.5px black solid;
+    cursor: ${(props) => (props.image !== '' ? 'pointer' : '')};
 `;
