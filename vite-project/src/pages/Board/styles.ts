3d7279258799af4cd98871c43b7d9a6ca1c62fa3
luizokada <luizfernandookada@gmail.com>
2023-02-27 21:04:14 -0300

    init: front end

---
+++ vite-project/src/pages/Board/styles.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+
+
+
+`
+
+export const BlackSquare = styled.div`
+width: 10px;
+height: 10px;
+background-color: #000;
+
+`;
+
+
+export const WhiteSquare = styled.div`
+width: 10px;
+height: 10px;
+background-color: white;
+
+`;
